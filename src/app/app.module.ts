bc2cc748f9b90cbf8713f40ae724807c3b4f4eb7
Sudeep Tuladhar <stuladhar@cardinalcommerce.com>
2018-04-03 15:00:00 -0400

    Fix Cors Issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule, Platform } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { HttpModule} from '@angular/http';
+
+import { HttpBackend, HttpXhrBackend } from '@angular/common/http';
+import { NativeHttpModule, NativeHttpBackend, NativeHttpFallback } from 'ionic-native-http-connection-backend';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { HttpModule} from '@angular/http';
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NativeHttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -26,7 +30,8 @@ import { HttpModule} from '@angular/http';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    {provide: HttpBackend, useClass: NativeHttpFallback, deps: [Platform, NativeHttpBackend, HttpXhrBackend]}
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {  Http, Headers, RequestOptions } from '@angular/http';
+import { HTTP } from '@ionic-native/http';
 import 'rxjs';
 
 @Component({
@@ -9,22 +9,12 @@ import 'rxjs';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController, public http: HTTP) {
     this.http = http;
   }
 
   getModal(){
     console.log('loading modal');
-
-    let options = new RequestOptions();
-    options.headers = new Headers();
-
-    options.headers.append("Content-Type", "application/json");
-    options.headers.append("Cache-Control", "no-store");
-    options.headers.append("Cache-Control", "no-cache");
-    options.headers.append("Access-Control-Allow-Origin", "*");
-    options.headers.append("Access-Control-Allow-Headers", "Content-Type");
-    // options.headers.append("Accept", "application/json");
 
    let body =  {
     "PaReq": "P.3bc0fde98430a7e81eca2b4d57b08bc806152a30a9fcc7e69b3ac118bde92f8445ee3b1e02c884d3a4c2aa680c67765b156f9945ecdd4c810742d7f760c5831e",
@@ -32,14 +22,17 @@ export class HomePage {
     "MD": "0_4dc64644-571b-49e3-9182-8cd1cf72a5d9"
    }
 
-   this.http.post("https://0eafstag.cardinalcommerce.com/EAFService/jsp/v1/redirect", body, options)
-    .map(res => res)
-    .subscribe(
-      data => console.log(data),
-      err => console.log('err', err),
-      () => console.log('yay'));
-   ;
-
+   this.http.post("https://0eafstag.cardinalcommerce.com/EAFService/jsp/v1/redirect", body, {})
+  .then(data => {
+    console.log(data.status);
+    console.log(data.data); // data received by server
+    console.log(data.headers);
+  })
+  .catch(error => {
+    console.log(error.status);
+    console.log(error.error); // error message as string
+    console.log(error.headers);
+  });
 
   }
 
